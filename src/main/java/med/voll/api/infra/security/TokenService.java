9fd4d4f892cc262ddfc53dc19c92d472563d8873
Marcelo Pena <coder.pena@gmail.com>
2026-02-15 11:59:38 -0300

    Deploy final.

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ import java.util.Date;
 public class TokenService {
 
     @Value("${api.security.token.secret}")
-    private  String secret;
+    private String secret;
 
     public String gerarToken(Usuario usuario) {
         try {
@@ -29,6 +30,22 @@ public class TokenService {
         } catch (JWTCreationException exception) {
             // Invalid Signing configuration / Couldn't convert Claims.
             throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    // specify any specific claim validations
+                    .withIssuer("API Voll.med")
+                    // reusable verifier instance
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            // Invalid signature/claims
+            throw new RuntimeException("Token JWT inválido ou expirado.");
         }
     }
 
